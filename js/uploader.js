694f2329c01da9a05e1b9451f2bec2eaa0408ad1
Phil Parsons <phil@geckoboard.com>
2013-02-09 18:41:43 +0000

    Finish annotating source code

--- js/uploader.js
+++ js/uploader.js
@@ -129,81 +129,106 @@
       up.startUpload(upload);
     }
     else {
-      // Add to the upload to the bottom of queue
+      // Add to the upload to the bottom of queue stack
       up.queue.unshift(upload);
     }
   };
 
+  // Starts a file upload and sets up all of the associated event handlers
   up.startUpload = function (upload) {
     var xhr = new XMLHttpRequest;
 
+    // Add a reference to the Ajax request on the upload object
     upload._xhr = xhr;
+    // And likewise add a reference to the upload object on the Ajax request
+    // and it's associated request upload so that it is easy to update the UI
+    // state in the event handlers
     xhr._file_upload = xhr.upload._file_upload = upload;
+    // Open the request and set the event listeners
     xhr.open('POST', 'upload.php', true);
     xhr.onload = up.uploadComplete;
     xhr.onerror = up.uploadError;
     xhr.upload.onprogress = up.updateProgressIndicator;
 
+    // Send the File object as the Ajax request body
     xhr.send(upload.file);
   };
 
+  // Event handler that runs when the file upload Ajax request has completed
   up.uploadComplete = function () {
     var li, nextUpload;
 
+    // Ensure that the file upload object set on the Ajax request referenced
+    // using the this keyword exists
     if (this._file_upload) {
+      // Get the UI list item so that it can be updated
       li = this._file_upload.listItem;
-      // Ensure progress bar is set to complete state
+      // Ensure the progress bar is set to complete state
       li.querySelector('.progress').style.width = '100%';
-      // Add complete class
+      // Add complete class to the list item
       li.classList.add('complete');
 
+      // Update the application state now that this upload has finished and 
+      // set the next upload to start
       up.clearUpload(this);
-      up.nextUpload();
     }
   };
 
+  // Update the state of the UI if the upload fails
   up.uploadError = function (ev) {
     var li;
 
+    // Ensure that the reference to the upload can be retrieved
     if (this._file_upload) {
+      // Set the error state of the list item by adding the error class
       li = this._file_upload.listItem;
       li.classList.add('error');
     }
 
+    // Clean up the application state
     up.clearUpload(this);
   };
 
+  // Update the uploads progress indicator with the percentage of the upload
+  // that has successfully uploaded
   up.updateProgressIndicator = function (ev) {
     var li, pctLoaded;
 
+    // Ensure the progress can be obtained from the event object
     if (ev.lengthComputable && this._file_upload) {
+      // Update the width of the progress bar in the list item to the 
+      // percentage of the file that has uploaded
       li = this._file_upload.listItem;
       pctLoaded = ((ev.loaded / ev.total) * 100);
       li.querySelector('.progress').style.width = pctLoaded + '%'
     }
   };
 
+  // Set the next upload to start, if it exists
   up.nextUpload = function () {
     var next = up.queue.pop();
 
+    // If there is a queued upload
     if (next) {
+      // Add it the list of active uploads and start it
       up.activeUploads.push(next);
       up.startUpload(next);
     }
   }
 
-  up.stopEvent = function (ev) {
-    ev.stopPropagation();
-    ev.preventDefault();
-  };
-
+  // Remove and return the upload with the matching id from the list of
+  // active uploads. If it does not exist undefined is returned
   up.removeActiveUploadById = function (uploadId) {
     var activeUpload;
 
+    // Check each upload to see if it has a matching id
     _.each(up.activeUploads, function (upload) {
+      // If the id matches then set the return variable and remove it from
+      // the list of active uploads
       if (upload.id === +uploadId) {
         activeUpload = upload;
         up.activeUploads.splice(up.activeUploads.indexOf(upload), 1);
+        // No further need to check any other active uploads
         return false;
       }
     });
@@ -211,27 +236,43 @@
     return activeUpload;
   };
 
-  // Filter the file list so only images get added
+  // Filter the file list and return an array of only the images
   up.filterImages = function (fileList) {
+    // Set up a Regex to check the file types agaist
     var acceptedType = /^image\//;
 
     // Test each files type to ensure it is an image of some type
     return _.filter(fileList, function (file) {
+      // If the test passes and it is an image add it to the filtered list
       return acceptedType.test(file.type);
     });
   };
 
-  // Remove the active upload and kills any references
+  // Remove the active upload and kill any references
   up.clearUpload = function (xhr) {
     var upload = xhr._file_upload;
 
+    // Ensure the reference to the file upload can be retrieved
     if (upload) {
+      // Remove the active upload
       up.activeUploads.splice(up.activeUploads.indexOf(upload), 1);
+      // Delete the reference on the upload to the Ajax request
       delete upload.xhr;
     }
 
+    // Delete the reference to the file upload object on the Ajax request
+    // and it's associated upload
     delete xhr._file_upload;
     delete xhr.upload._file_upload;
+
+    // Start the next upload
+    up.nextUpload();
+  };
+
+  // Stop the default action of an event and prevent it propagating
+  up.stopEvent = function (ev) {
+    ev.stopPropagation();
+    ev.preventDefault();
   };
 
   // Do it!
